5946d1c96afc708c083e4fe51435a98d5b221b09
avisrabon22 <avisrabon22@gmail.com>
2024-04-29 22:31:26 +0530

    update validation

--- src/Component/DashboardComponent.jsx
+++ src/Component/DashboardComponent.jsx
@@ -1,8 +1,14 @@
+import { useEffect } from "react"
 import { NavBar } from "./NavBar"
 
 export const DashboardComponent = ()=> {
 
-    
+    useEffect(()=>{
+        
+       
+    }
+    ,[])
+
     return <>
     <NavBar/>
     <div className="container mx-auto">
